01507f0888b18890eadcb445c459a7a0457aa7fb
Stanislav <stas.mitryushin@gmail.com>
2022-02-13 14:15:59 +0300

    debug

--- backend/api_foodgram/api_foodgram/settings.py
+++ backend/api_foodgram/api_foodgram/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'backend', '62.84.118.66']
 
